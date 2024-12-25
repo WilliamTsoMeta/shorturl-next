b2e181d07233e09651a0454b663fda9855b78da2
William Tso <vitalitymailg@gmail.com>
2024-12-25 21:11:47 +0800

    use next ts config file

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,32 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+    ],
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.plugins.push(
+        new MiniCssExtractPlugin({
+          filename: 'static/css/[name].[contenthash].css',
+          chunkFilename: 'static/css/[id].[contenthash].css',
+        })
+      );
+    }
+    return config;
+  },
+}
 
 export default nextConfig;
